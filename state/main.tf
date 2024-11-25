e7aadb1e5369730656903fa5c0d984e7438d3ee1
IanOliv <22185362+IanOliv@users.noreply.github.com>
2024-11-25 18:59:55 -0300

    feature(state): add GitHub Actions variable for app installation organization ID

--- state/main.tf
+++ state/main.tf
@@ -33,6 +33,11 @@ resource "github_actions_variable" "github_variable_gh_app_id" {
   variable_name     = "GH_APP_ID"
   value = var.GH_APP_ID
 }
+resource "github_actions_variable" "github_variable_gh_app_installation" {
+  repository      = data.github_repository.github.name
+  variable_name     = "GH_APP_INSTALLATION_ORGANIZATION_ID"
+  value = var.GH_APP_INSTALLATION_ORGANIZATION_ID
+}
 resource "github_actions_secret" "github_secret_gh_app_private_key" {
   repository      = data.github_repository.github.name
   secret_name     = "GH_APP_PRIVATE_KEY"
