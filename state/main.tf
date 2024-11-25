a8912cc338eb6d4624bc0561ecda16bf84c77881
Ian Gabriel Oliveira de Sousa <22185362+IanOliv@users.noreply.github.com>
2024-11-25 17:35:23 -0300

    feature(state#3)
    * chore: add .gitignore to exclude terraform state files
    
    * feature: add Terraform configuration for GitHub provider
    
    * chore: update .gitignore to exclude additional Terraform state files and directories
    
    * feature: add variables for GitHub App configuration
    
    * feature: update GitHub provider configuration and add actions variables and secrets

---
+++ state/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+
+  }
+  required_version = ">= 1.0"
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "github" {
+  owner = var.GITHUB_ORGANIZATION
+  app_auth {
+    id              = var.GH_APP_ID
+    installation_id = var.GH_APP_INSTALLATION_ORGANIZATION_ID
+    pem_file        = file(var.GH_APP_PRIVATE_KEY_PATH)
+  }
+}
+
+data "github_repository" "github" {
+  name = ".github"
+}
+
+
+resource "github_actions_variable" "github_variable_gh_app_id" {
+  repository      = data.github_repository.github.name
+  variable_name     = "GH_APP_ID"
+  value = var.GH_APP_ID
+}
+resource "github_actions_secret" "github_secret_gh_app_private_key" {
+  repository      = data.github_repository.github.name
+  secret_name     = "GH_APP_PRIVATE_KEY"
+  plaintext_value = file(var.GH_APP_PRIVATE_KEY_PATH)
+}
