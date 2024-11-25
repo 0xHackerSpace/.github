e1600a53c0697c2a87aea501b3485a0aa9ba8971
IanOliv <22185362+IanOliv@users.noreply.github.com>
2024-11-25 17:29:55 -0300

    feature: add variables for GitHub App configuration

---
+++ state/variables.tf
@@ -0,0 +1,26 @@
+# GitHub App
+
+variable "GH_APP_ID" {
+  description = "GitHub App ID"
+  type        = string
+}
+
+variable "GH_APP_PRIVATE_KEY" {
+  description = "GitHub App ID"
+  type        = string
+}
+
+variable "GH_APP_PRIVATE_KEY_PATH" {
+  description = "GitHub App ID"
+  type        = string
+}
+
+variable "GH_APP_INSTALLATION_ORGANIZATION_ID" {
+  description = "GitHub App Installation ID"
+  type        = string
+}
+
+variable "GITHUB_ORGANIZATION" {
+  description = "GitHub organization name"
+  type        = string
+}
