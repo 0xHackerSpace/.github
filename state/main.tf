56adfb6d4a954ac124a0eb265fa736589cea1bf4
IanOliv <22185362+IanOliv@users.noreply.github.com>
2024-11-25 19:01:46 -0300

    feature(workflows): update GitHub Actions variable for app installation organization ID

--- state/main.tf
+++ state/main.tf
@@ -41,5 +41,5 @@ resource "github_actions_variable" "github_variable_gh_app_installation" {
 resource "github_actions_secret" "github_secret_gh_app_private_key" {
   repository      = data.github_repository.github.name
   secret_name     = "GH_APP_PRIVATE_KEY"
-  plaintext_value = file(var.GH_APP_PRIVATE_KEY_PATH)
+  plaintext_value = var.GH_APP_PRIVATE_KEY
 }
