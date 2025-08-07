743e4c75e2d2539099a92cbcfe202a3747cc9ebb
IanOliv <22185362+IanOliv@users.noreply.github.com>
2025-08-07 01:54:46 -0300

    feat: add wrapper base config

---
+++ state/wrapper.tf
@@ -0,0 +1,19 @@
+resource "github_repository" "wrapper" {
+  name        = "wrapper"
+  description = "Wrapper repository for managing infrastructure"
+  visibility  = "public"
+
+  has_issues    = true
+  has_wiki      = false
+  has_projects  = false
+  has_downloads = false
+
+  allow_merge_commit = true
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init         = true
+
+  delete_branch_on_merge = true
+  
+  topics = ["infrastructure", "terraform", "wrapper"]
+}
